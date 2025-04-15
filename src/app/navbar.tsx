7d75ed58b8b4a94dea58582a12d9f2e19ff6d5ce
Aman Raj <amanraj56369509@gmail.com>
2025-04-15 16:59:57 +0530

    Navbar updated

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,9 +1,32 @@
-"use client";
-import Link from 'next/link'
-import React from 'react'
+
+
+
+
+'use client';
+
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth } from '@/lib/firebase';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 
 const Navbar = () => {
+  const [user, setUser] = useState<any>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    router.push('/login');
+  };
 
   return (
     <nav className="fixed top-0 left-0 w-full z-20 
@@ -11,16 +34,28 @@ const Navbar = () => {
     shadow-lg border-b border-white/30 
     flex justify-between items-center px-6 py-3
   ">
-    <Link href={'/'} className="text-2xl font-bold text-white">
+      <Link href={'/'} className="text-2xl font-bold text-white">
       MyGlassNav
     </Link>
-    <div className="space-x-4 text-white">
-      <Link href={"/login"} className="font-bold  hover:scale-105 " >Login</Link>
-      <Link href={"/login"} className="font-bold  hover:scale-105 " >Sign Up</Link>
-    </div>
-  </nav>
-  
-  )
+
+      <div className="flex gap-4 items-center text-white">
+        {!user ? (
+          <>
+            <Link href="/home" className="hover:underline font-bold  hover:scale-105">Explore</Link>
+            
+            <Link href="/login" className="hover:underline font-bold  hover:scale-105">Signup</Link>
+          </>
+        ) : (
+          <>
+            <Link href="/contest" className=" hover:underline font-bold  hover:scale-105">Contest</Link>
+            <button onClick={handleLogout} className="hover:underline font-bold  hover:scale-105">Logout</button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+
